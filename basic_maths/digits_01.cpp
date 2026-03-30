06294a936a95c5206342c54b6931fc31ddebaa69
srijan-aich <srijanaich22@gmail.com>
2026-03-30 17:39:00 +0000

    Add basic maths stuff

--- basic_maths/digits_01.cpp
+++ basic_maths/digits_01.cpp
@@ -2,12 +2,11 @@ int count(int n) {
     int cnt = 0;
 
     while (n > 0) {
-        int lastDigit = n % 10; // no use of lastDigit in counting digits
+        int lastDigit = n % 10; // no use of lastDigit in counting digits , we can remove it and directly do cnt = cnt + 1; in the loop
         cnt = cnt + 1;
         n = n / 10;
     }
-
     return cnt;
-}
+} 
 
 // this will not run , just for understanding the concept of counting digits in a number
