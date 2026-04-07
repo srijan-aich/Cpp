38d04d9c7a4fa1b4c462d0ee2e6305bd239d7c85
srijan-aich <srijanaich22@gmail.com>
2026-04-07 12:44:46 +0000

    add basic math programs

--- basic_maths/print_divisors.cpp
+++ basic_maths/print_divisors.cpp
@@ -1,19 +1,35 @@
-#include <iostream>
+#include<bits/stdc++.h>
+
 using namespace std;
 
 void printDivisors(int n) {
-    for (int i = 1; i <= n; i++) {
+    vector<int> ls;
+    
+    // Loop from 1 to the square root of n
+    for (int i = 1; i <= sqrt(n); i++) {
         if (n % i == 0) {
-            cout << i << " ";
+            // Add the divisor i
+            ls.push_back(i);
+            
+            // If the counterpart divisor (n/i) is different from i, add it too
+            if ((n / i) != i) {
+                ls.push_back(n / i);
+            }
         }
+    }
+    
+    // Sort the divisors in ascending order
+    sort(ls.begin(), ls.end());
+    
+    // Print the sorted divisors
+    for (auto it : ls) {
+        cout << it << " ";
     }
 }
 
 int main() {
     int n;
     cin >> n;
-
     printDivisors(n);
-
     return 0;
 }
