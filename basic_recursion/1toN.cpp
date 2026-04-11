13b5c9b465dd0164a4874a37cb887a56632239ae
srijan-aich <srijanaich22@gmail.com>
2026-04-11 16:59:26 +0000

    add basic recursion programs

---
+++ basic_recursion/1toN.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void printNumbers(int i, int n) {
+    // BASE CONDITION
+    // Stop once i goes past n
+    if (i > n) {
+        return;
+    }
+
+    // WORK
+    // Print the current value of i
+    cout << i << " ";
+
+    // RECURSIVE CALL
+    // Move to the next number
+    printNumbers(i + 1, n);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    // Start from 1
+    printNumbers(1, n);
+
+    return 0;
+}
